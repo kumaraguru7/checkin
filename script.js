dd4a9505d5aba6941db4cd68b5f18c535d4d6347
Kumaraguru <54850140+kumaraguru7@users.noreply.github.com>
2020-05-04 02:44:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var loginWindow = document.querySelector('.login-window');
-  var loginWindowHeaderMessage = document.querySelector('.login-window__header-message');
-  var loginWindowContentWrapper = document.querySelector('.wrapper');
-  var loginButton = document.querySelector('.login-window__login-button > button');
+// correct password is `password` lol
 
-  var inputAccount = document.querySelector('.login-window__form > input[type="text"]');
-  var inputPassword = document.querySelector('.login-window__form > input[type="password"]');
+const $ = (s, o = document) => o.querySelector(s);
+const $$ = (s, o = document) => o.querySelectorAll(s);
 
-  var wrapperDimension = Math.sqrt(Math.pow(loginWindow.clientWidth, 2) + Math.pow(loginWindow.clientHeight, 2)) + 'px';
+const login = $('#login-form');
+const passwordContainer = $('.password', login);
+const password = $('input', passwordContainer);
+const passwordList = $('.dots', passwordContainer);
+const submit = $('button', login);
 
-  loginWindowContentWrapper.style.width = wrapperDimension;
-  loginWindowContentWrapper.style.height = wrapperDimension;
+password.addEventListener('input', e => {
+    if(password.value.length > $$('i', passwordList).length) {
+        passwordList.appendChild(document.createElement('i'));
+    }
+    submit.disabled = !password.value.length;
+    passwordContainer.style.setProperty('--cursor-x', password.value.length * 10 + 'px');
+});
 
-  loginButton.onclick = function () {
-    loginWindow.classList.add('loading');
+let pressed = false;
 
-    setTimeout(function () {
-      // Login result callback
-      loginWindow.classList.remove('loading');
+password.addEventListener('keydown', e => {
 
-      if (inputAccount.value == 'abc' && inputPassword.value == '123') {
-        // Login success         
-        loginWindow.classList.add('fadeout');
-      } else
-      {
-        loginWindowHeaderMessage.style.display = 'block';
-      }
+    if(pressed || login.classList.contains('processing') || (password.value.length > 14 && e.keyCode != 8 && e.keyCode != 13)) {
+        e.preventDefault();
+    }
+    pressed = true;
 
-    }, 4000);
-  };
+    setTimeout(() => pressed = false, 50);
+
+    if(e.keyCode == 8) {
+        let last = $('i:last-child', passwordList);
+        if(last !== undefined && last) {
+            last.classList.add('remove');
+            setTimeout(() => last.remove(), 50);
+        }
+    }
+
+});
+
+password.addEventListener('select', function() {
+    this.selectionStart = this.selectionEnd;
+});
+
+login.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    if(!login.classList.contains('processing')) {
+        login.classList.add('processing');
+        setTimeout(() => {
+
+            let cls = password.value == 'password' ? 'success' : 'error';
+            console.log(password.value);
+
+            login.classList.add(cls);
+            setTimeout(() => {
+                login.classList.remove('processing', cls);
+                if(cls == 'error') {
+                    password.value = '';
+                    passwordList.innerHTML = '';
+                    submit.disabled = true;
+                }
+            }, 2000);
+            setTimeout(() => {
+                if(cls == 'error') {
+                    passwordContainer.style.setProperty('--cursor-x', 0 + 'px');
+                }
+            }, 600);
+
+        }, 1500);
+    }
+
 });
